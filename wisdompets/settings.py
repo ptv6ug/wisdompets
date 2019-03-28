436a46e18215b436450d86dcfab75a599ab033a5
Pauline Vu <ptv6ug@virginia.edu>
2019-03-28 11:36:01 -0400

    all

--- wisdompets/settings.py
+++ wisdompets/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pqype_-t3)@dn9dvi*^20rocxtdj49ooza$!$6pc@3sp8tc41r'
+SECRET_KEY = 'xigekntf99wnyz5(tuqxo)jm!za=*^6z3^9lwh_%c8ndxa^^-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -8,5 +7,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
-
 ]
